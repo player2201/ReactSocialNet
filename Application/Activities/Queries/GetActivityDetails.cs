bfcfda5b26c182738c2afe345f355ccd0d0e6ff1
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2026-01-28 14:54:53 +0200

    Added Mediator handler for an activity details

--- Application/Activities/Queries/GetActivityDetails.cs
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Diagnostics;
+using Domain;
 using MediatR;
+using Persistence;
 
 namespace Application.Activities.Queries;
 
@@ -8,6 +9,20 @@ public class GetActivityDetails
 {
     public class Query : IRequest<Activity>
     {
-        public string Id { get; set; }
+        public required string Id { get; set; }
+    }
+
+    public class Handler(AppDbContext context) : IRequestHandler<Query, Activity>
+    {
+        public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var activity = await context.Activities.FindAsync([request.Id], cancellationToken);
+
+            if(activity == null)
+            
+                throw new Exception("Activity not found");
+            
+            return activity;
+        }
     }
 }
