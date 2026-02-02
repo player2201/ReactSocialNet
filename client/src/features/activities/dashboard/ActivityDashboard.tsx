21033f50bacc63e8ba7583112f0c93e2f26ae13e
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2026-02-02 15:39:22 +0200

    ActivityDetails view created

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,18 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
 type Props = {
   activities: Activity[];
 };
 
 export default function ActivityDashboard({ activities }: Props) {
   return (
-    <Grid>
-      <Grid size={9}>
+    <Grid container spacing={3}>
+      <Grid size={7}>
         <ActivityList activities={activities} />
+      </Grid>
+      <Grid size={5}>
+        {activities[0] && <ActivityDetails activity={activities[0]} />}
       </Grid>
     </Grid>
   );
