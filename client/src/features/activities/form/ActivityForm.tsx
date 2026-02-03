3fec1d1e62fad451b6f1c215c99674314b4475f6
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2026-02-03 16:10:44 +0200

    ActivityForm input submission basics added.

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import type { FormEvent } from "react";
 
 type Props = {
   activity?: Activity;
@@ -6,23 +7,53 @@ type Props = {
 };
 
 export default function ActivityForm({ activity, closeForm }: Props) {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data: { [key: string]: FormDataEntryValue } = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+    console.log(data);
+  };
   return (
     <Paper sx={{ borderRadius: 3, padding: 3 }}>
       <Typography variant="h5" gutterBottom color="primary">
         Create Activity
       </Typography>
-      <Box component={"form"} display={"flex"} flexDirection={"column"} gap={3}>
-        <TextField label="Title" value={activity?.title}></TextField>
-        <TextField label="Description" multiline rows={3}></TextField>
-        <TextField label="Category"></TextField>
-        <TextField label="Date" type="date"></TextField>
-        <TextField label="City"></TextField>
-        <TextField label="Venue"></TextField>
+      <Box
+        component={"form"}
+        onSubmit={handleSubmit}
+        display={"flex"}
+        flexDirection={"column"}
+        gap={3}
+      >
+        <TextField name="title" label="Title" defaultValue={activity?.title} />
+        <TextField
+          name="description"
+          label="Description"
+          multiline
+          rows={3}
+          defaultValue={activity?.description}
+        />
+        <TextField
+          name="category"
+          label="Category"
+          defaultValue={activity?.category}
+        />
+        <TextField
+          name="date"
+          label="Date"
+          type="date"
+          defaultValue={activity?.date}
+        />
+        <TextField name="city" label="City" defaultValue={activity?.city} />
+        <TextField name="venue" label="Venue" defaultValue={activity?.venue} />
         <Box display={"flex"} justifyContent={"end"} gap={3}>
           <Button onClick={closeForm} color="inherit">
             Cancel
           </Button>
-          <Button color="success" variant="contained">
+          <Button color="success" variant="contained" type="submit">
             Submit
           </Button>
         </Box>
