246ae0006c0a0ef37f40efe5d86f7821eaa974db
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2026-02-03 19:04:42 +0200

    Delete activity funtion added on client side

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -50,6 +50,10 @@ function App() {
     setEditMode(false);
   };
 
+  const handleDelete = (id: string) => {
+    setActivities(activities.filter((x) => x.id !== id));
+  };
+
   return (
     <Box sx={{ bgcolor: "#eeeeee" }}>
       <CssBaseline />
@@ -64,6 +68,7 @@ function App() {
           openForm={handleOpenForm}
           closeForm={handleFormClose}
           submitForm={handleSubmitForm}
+          deleteActivity={handleDelete}
         />
       </Container>
     </Box>
