eba983c8ef686ca6dda05bad91aff296d000e032
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2026-02-04 15:04:41 +0200

    Created a bse Axios instance in agent.ts

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,14 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import agent from "../api/agent";
 
 //react hooks by conventions need to have the word 'use' at the start
 export const useActivities = () => {
   const { data: activities, isPending } = useQuery({
     queryKey: ["activities"],
     queryFn: async () => {
-      const response = await axios.get<Activity[]>(
-        "https://localhost:5001/api/activities",
-      );
+      const response = await agent.get<Activity[]>("/activities");
       return response.data;
     },
   });
